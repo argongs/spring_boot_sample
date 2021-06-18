62dccb5f40bf1c4da3870d6789c713937ffc57dc
argongs <gauravisvirtual@yahoo.com>
2021-06-18 10:20:40 +0530

    removed GenerationType in favour of automated String type id generation

--- src/main/java/com/college/data/course/Course.java
+++ src/main/java/com/college/data/course/Course.java
@@ -10,7 +10,6 @@ import javax.persistence.Id;
 public class Course {
     
     @Id
-    @GeneratedValue (strategy = GenerationType.AUTO)
     private String id;
     @Column (nullable = false)
     private String name;
