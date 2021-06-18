d06bc1787edac43bdb3d01cbfd0b3a6006883a1f
argongs <gauravisvirtual@yahoo.com>
2021-06-18 11:04:37 +0530

    added a @GenericGenerator for automated id generation of string type ids

--- src/main/java/com/college/data/professor/Professor.java
+++ src/main/java/com/college/data/professor/Professor.java
@@ -2,12 +2,17 @@ package com.college.data.professor;
 
 import javax.persistence.Column;
 import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
 import javax.persistence.Id;
+
+import org.hibernate.annotations.GenericGenerator;
 
 @Entity
 public class Professor {
     
     @Id
+    @GeneratedValue (generator = "system-uuid")
+    @GenericGenerator (name = "system-uuid", strategy = "uuid")
     private String id;
     @Column (nullable = false)
     private String name;
--- src/main/java/com/college/data/student/Student.java
+++ src/main/java/com/college/data/student/Student.java
@@ -3,14 +3,16 @@ package com.college.data.student;
 import javax.persistence.Column;
 import javax.persistence.Entity;
 import javax.persistence.GeneratedValue;
-import javax.persistence.GenerationType;
 import javax.persistence.Id;
+
+import org.hibernate.annotations.GenericGenerator;
 
 @Entity
 public class Student {
     
     @Id
-    @GeneratedValue (strategy = GenerationType.AUTO)
+    @GeneratedValue (generator = "system-uuid")
+    @GenericGenerator (name = "system-uuid", strategy = "uuid")
     private String id;
     @Column (nullable = false)
     private String name;
